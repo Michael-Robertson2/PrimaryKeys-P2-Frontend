4cd87a907fcbd93e6e7de06502ab6d34a947baf6
Yuan Cao <yuan876@revature.net>
2022-12-14 14:16:12 -0500

    Change FeedPage to make use of the new Post component

--- my-app/src/pages/FeedPage.tsx
+++ my-app/src/pages/FeedPage.tsx
@@ -3,7 +3,11 @@ import Post from "../components/Post";
 function FeedPage(){
     return (
         <div className="flex flex-col items-center border-double border-4 border-red-500">
-        <Post/>
+        <Post id="ANYTHING GOES" 
+                name="Elias Gonzalez" 
+                icon="IGNORED" 
+                date="04 / 02 / 2022" 
+                message="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula."/>
         </div>
     );
 }
