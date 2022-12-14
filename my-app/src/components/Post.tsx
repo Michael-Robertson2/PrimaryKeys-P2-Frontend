66ebf5703ed6b99a46a1c96bb2d834da4ecbb087
Yuan Cao <yuan876@revature.net>
2022-12-14 14:15:45 -0500

    Change Post componenent to take in a PostContent argument for preparation for pulling data from backend

--- my-app/src/components/Post.tsx
+++ my-app/src/components/Post.tsx
@@ -3,28 +3,27 @@ import { PencilSquareIcon } from '@heroicons/react/24/solid'
 import { HandThumbUpIcon } from '@heroicons/react/24/solid'
 import { UserIcon } from '@heroicons/react/24/solid'
 
-function Post() {
-    return (
-        <ol className="pt-8">
-            {/* if u need to make a list of posts, create inside OL (ordered list), each post is an INDIVIDUAL li (list item) */}
-            <li>
-                <div className="md:flex flex-start">
-                    <div className="block p-6 rounded-lg shadow-lg bg-gray-100  ml-6 mr-6 mb-10">
-                        <div className="flex justify-between mb-4">
-                            <div>
-                                <UserIcon className="inline-block h-5 pr-2"/>
-                                <a href="#!" className="inline-block font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm">Elias Gonzalez</a>    
-                            </div>
+import PostContent from "../models/PostContent";
 
-                            <a href="#!" className="font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm">04 / 02 / 2022</a>
+function Post(post: PostContent) {
+    return (
+        <li className="pt-8">
+            <div className="md:flex flex-start">
+                <div className="block p-6 rounded-lg shadow-lg bg-gray-100  ml-6 mr-6 mb-10">
+                    <div className="flex justify-between mb-4">
+                        <div>
+                            <UserIcon className="inline-block h-5 pr-2"/>
+                            <a href="#!" className="inline-block font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm"> { post.name } </a>    
                         </div>
-                        <p className="text-gray-700 mb-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque scelerisque diam non nisi semper, et elementum lorem ornare. Maecenas placerat facilisis mollis. Duis sagittis ligula in sodales vehicula.</p>
-                        <PencilSquareIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
-                        <HandThumbUpIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
+
+                        <a href="#!" className="font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm"> { post.date } </a>
                     </div>
+                    <p className="text-gray-700 mb-6"> {post.message} </p>
+                    <PencilSquareIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
+                    <HandThumbUpIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
                 </div>
-            </li>
-        </ol>
+            </div>
+        </li>
     )
 }
 
