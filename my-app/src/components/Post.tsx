a559e5eb578a0840937e607bc21adcd2f045335a
Yuan Cao <yuan876@revature.net>
2022-12-15 12:51:18 -0500

    Further simplify Post component for later reuse, and implement feed with unique key

--- my-app/src/components/Post.tsx
+++ my-app/src/components/Post.tsx
@@ -7,23 +7,19 @@ import PostContent from "../models/PostContent";
 
 function Post(post: PostContent) {
     return (
-        <li className="pt-8">
-            <div className="md:flex flex-start">
-                <div className="block p-6 rounded-lg shadow-lg bg-gray-100  ml-6 mr-6 mb-10">
-                    <div className="flex justify-between mb-4">
-                        <div>
-                            <UserIcon className="inline-block h-5 pr-2"/>
-                            <a href="#!" className="inline-block font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm"> { post.name } </a>    
-                        </div>
-
-                        <a href="#!" className="font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm"> { post.date } </a>
-                    </div>
-                    <p className="text-gray-700 mb-6"> {post.message} </p>
-                    <PencilSquareIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
-                    <HandThumbUpIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
+        <div className="block p-6 rounded-lg shadow-lg bg-gray-100">
+            <div className="flex justify-between mb-4">
+                <div>
+                    <UserIcon className="inline-block h-5 pr-2"/>
+                    <a href="#!" className="inline-block font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm"> { post.name } </a>    
                 </div>
+
+                <a href="#!" className="font-medium text-teal-600 hover:text-teal-900 focus:text-slate-400 duration-300 transition ease-in-out text-sm"> { post.date } </a>
             </div>
-        </li>
+            <p className="text-gray-700 mb-6"> {post.message} </p>
+            <PencilSquareIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
+            <HandThumbUpIcon className="inline-block h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" />
+        </div>
     )
 }
 
