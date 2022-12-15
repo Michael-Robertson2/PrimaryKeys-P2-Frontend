61e8f9c98af1900dad67f81fee75db33c06fda69
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-12-14 22:27:16 -0600

    Update LoginPage.tsx

--- my-app/src/pages/LoginPage.tsx
+++ my-app/src/pages/LoginPage.tsx
@@ -1,22 +1,32 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { FormEvent, useState } from 'react';
 import SylvesterAPI from './ApiConfig';
+
 
 function LoginPage(){
     const [username, setUsername] = useState<string>("");
     const [password, setPassword] = useState<string>("");
-
+    const [error, setError] = useState<string>("");
+    const navigate = useNavigate();
 
     async function submit(e: FormEvent){
         e.preventDefault();
         await SylvesterAPI.post("/auth",{
             username: username,
             password: password
-        }).then((response) => {console.log(response)})
-            .catch((error) => {console.log(error)});
-        
+        }).then((response) => {
+            setError("")
+            console.log(response.data)
+            navigate("/")
+        })
+            .catch((error) => {
+                setError(error.response.data.message)
+                setTimeout(() =>setError(""),5000)
+            });
+
         //console.log("Client attempted login: (Username: " + username + " Password: " + password+ ")");
-        
+        setPassword("")
+        setUsername("")
 
     }
 
@@ -34,6 +44,8 @@ function LoginPage(){
                 <input className="bg-gray-100 shadow-xl rounded-md px-5 py-2" type="text" placeholder="Username" onChange={(e)=> setUsername(e.target.value)}/>
                 <input className="bg-gray-100 shadow-xl rounded-md px-5 py-2"  type="password" placeholder="Password" onChange={(e)=> setPassword(e.target.value)}/>
                 <button className="bg-slate-800 rounded-md text-white mt-2 px-5 py-2 ease-out duration-300 hover:scale-110">LOGIN</button>
+                
+                { error ? <p className='text-red-600'>{error}</p>: null }
                 <Link to={"/signup"} className="text-blue-700 underline">Create new account</Link>
             </div>
         </form>
