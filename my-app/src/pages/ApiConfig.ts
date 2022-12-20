73e90965c448ba164a48627af060eb24c9f10b0f
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-12-20 16:16:11 -0600

    Update ApiConfig.ts

--- my-app/src/pages/ApiConfig.ts
+++ my-app/src/pages/ApiConfig.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const SylvesterAPI = axios.create({
-    baseURL: "http://localhost:8081/sylvester",
+    baseURL: "http://localhost:8080/sylvester",
     headers: {
         "Content-Type": "application/json"
     }
