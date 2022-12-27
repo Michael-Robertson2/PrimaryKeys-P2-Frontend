9316351135141ce0b19dc6808c7bba6b80959f06
ScruffyC <118226061+ScruffyC@users.noreply.github.com>
2022-12-27 11:34:10 -0600

    Update ProfilePage.tsx

--- my-app/src/pages/ProfilePage.tsx
+++ my-app/src/pages/ProfilePage.tsx
@@ -64,42 +64,41 @@ function ProfilePage(){
     }
 
     return (
-        <form onSubmit={(e)=>submit(e)} className="flex justify-center items-center" >
+        <form onSubmit={(e)=>submit(e)} >
             <div className=" flex flex-row border-solid border-4 h-full shadow-md bg-white px-1">
-                <div className="flex flex-col w-1/5 items-center border-solid border-4 border-cyan-300 ">
-
-                    <input className="bg-gray-100 shadow-xl rounded-md px-5 py-2" placeholder={profile?.profilePicUrl} value={profilePicUrl} onChange={(e)=>setProfilePicUrl(e.target.value)} />
-                    {profile === null ? <UserIcon className="" /> : (
-                        profile.profilePicUrl === null ? "" : <img src={profile.profilePicUrl} alt="something" />
-                    )}
-                    <ul>
-                            <li>{principal?.username}</li>
-                            <li><input className="bg-gray-100 shadow-xl rounded-md px-5 py-2" placeholder={profile?.displayName} value={displayName} onChange={(e)=>setDisplayName(e.target.value)} /></li>
-                            <li><div>
-                                <p className='inline-block pr-5'>Birth Date</p>
-                                <input type="date" className="bg-gray-100 shadow-xl rounded-md px-5 py-2"  placeholder="Date of Birth" value={birthDate} onChange={(e)=>setBirthDate(e.target.value)} />
-                            </div></li>
-                            <li><input className="bg-gray-100 shadow-xl rounded-md px-5 py-2"  placeholder={profile?.occupation} value={occupation} onChange={(e)=>setOccupation(e.target.value)} /></li>
-                            <li><input className="bg-gray-100 shadow-xl rounded-md px-5 py-2"  placeholder={profile?.location} value={location} onChange={(e)=>setLocation(e.target.value)}/></li>
-                    </ul>
-                </div>
-
-
-                <div className="border-solid border-4 w-full">
-                    <h1>Bio</h1>
-                    <div className="border-solid border-4">
-                        <input className="bg-gray-100 shadow-xl rounded-md px-5 py-2"  placeholder={profile?.bio} value={bio} onChange={(e)=>setBio(e.target.value)}/>
-                    </div>
-                    
-                    <h1>My Post</h1>
-                    { /*The bottom dive is the get request for our post and then mapped out */ }
-                    <div className="border-solid border-4">
-                        Container2.1 PostHere
-                    </div>
-                </div>
-
-                { hasUpdates ? <button className="bg-slate-800 rounded-md text-white mt-2 px-5 py-2 ease-out duration-300 hover:scale-110" onClick={ toggleOff }>Update</button> : <></> }
+                <input className="bg-gray-100 shadow-xl rounded-md" placeholder={profile?.profilePicUrl} value={profilePicUrl} onChange={(e)=>setProfilePicUrl(e.target.value)} />
+                {profile === null ? <UserIcon className="" /> : (
+                    profile.profilePicUrl === null ? "" : <img src={profile.profilePicUrl} alt="something" />
+                )}
+                <ul>
+                        <li>{principal?.username}</li>
+                        <li><input className="bg-gray-100 shadow-xl rounded-md" placeholder={profile?.displayName} value={displayName} onChange={(e)=>setDisplayName(e.target.value)} /></li>
+                        <li><div>
+                            <p className='inline-block pr-5'>Birth Date</p>
+                            <input type="date" className="bg-gray-100 shadow-xl rounded-md"  placeholder="Date of Birth" value={birthDate} onChange={(e)=>setBirthDate(e.target.value)} />
+                        </div></li>
+                        <li><input className="bg-gray-100 shadow-xl rounded-md"  placeholder={profile?.occupation} value={occupation} onChange={(e)=>setOccupation(e.target.value)} /></li>
+                        <li><input className="bg-gray-100 shadow-xl rounded-md"  placeholder={profile?.location} value={location} onChange={(e)=>setLocation(e.target.value)}/></li>
+                </ul>
             </div>
+
+
+            <div className="border-solid border-4 w-full">
+                <h1>Bio</h1>
+                <div className="border-solid border-4">
+                    <input className="bg-gray-100 shadow-xl rounded-md"  placeholder={profile?.bio} value={bio} onChange={(e)=>setBio(e.target.value)}/>
+                </div>
+                
+                <h1>My Post</h1>
+                { /*The bottom dive is the get request for our post and then mapped out */ }
+                <div className="border-solid border-4">
+                    Container2.1 PostHere
+                </div>
+            </div>
+
+            { hasUpdates ? <button className="bg-slate-800 rounded-md text-white ease-out duration-300 hover:scale-110" onClick={ toggleOff }>Update</button> : <></> }
+            { error ? <p className='text-red-600'>{error}</p>: null }
+            <Link to={"/"} className="text-blue-700 underline">Cancel </Link>
         </form>
     );
 }
