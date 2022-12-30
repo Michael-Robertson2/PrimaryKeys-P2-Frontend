02a8833afc9d26a17528422de33b2d5dc75bad5c
Yuan Cao <yuan876@revature.net>
2022-12-29 20:28:16 -0500

    Fixed commenting tenor not being reset

--- my-app/src/components/MakeComment.tsx
+++ my-app/src/components/MakeComment.tsx
@@ -2,10 +2,13 @@ import { useContext, useState } from 'react';
 import SylvesterAPI from '../utils/ApiConfig';
 import TenorSearch from './TenorSearch';
 import { PrincipalContext } from '../context/PrincipalProvider';
+import { GifIcon } from '@heroicons/react/24/outline'
+import { PencilSquareIcon } from '@heroicons/react/24/solid'
 
 function MakeComment(props: any) {
     const [reply, setReply] = useState<string>("");
     const [tenorUrl, setTenorUrl] = useState<string>("");
+    const [tenorState, setTenorState] = useState<boolean>(false);
     const principal = useContext(PrincipalContext); 
 
     //request comments from parent post
@@ -38,15 +41,24 @@ function MakeComment(props: any) {
 
         console.log("GOT HERE");
         setReply("");
-        setTenorUrl("");
+        toggleTenor()
         console.log("GOT HERE");
+    }
+
+    function toggleTenor() {
+        setTenorState(!tenorState);
     }
 
     return (
         <form className="block p-6 rounded-lg shadow-lg bg-gray-200 w-full">
-            <input className="w-full" placeholder="I tawt I taw a puddy tat!" value={ reply } onChange={(e) => setReply(e.target.value)}></input>
-            <TenorSearch passData={setTenorUrl}/>
-            <button onClick={ (e) => submit(e) } type="submit">Submit</button>
+            <input className="w-full mb-6" placeholder="I tawt I taw a puddy tat!" value={ reply } onChange={(e) => setReply(e.target.value)}></input>
+            
+            {tenorState ? <TenorSearch passData={setTenorUrl}/> : <div></div>}
+
+            <div className="border-double border-4 flex justify-end">            
+                <GifIcon className="h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" onClick={toggleTenor}/>
+                <PencilSquareIcon className="h-6 pr-2 hover:opacity-40 transition duration-150 ease-in-out" onClick={submit}/>
+            </div>
         </form>
     )
 }
